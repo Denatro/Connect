c9abb1a2f549e6beb22b017296412d9cedececdc
Denatro <73163499+Denatro@users.noreply.github.com>
2022-05-06 12:36:45 +0900

    Add files via upload

---
+++ connect/script.js
@@ -0,0 +1,17 @@
+﻿var x=0; a=0;
+vpered.onclick=function(){
+	x=x-100;
+	if(x<=-300)
+		x=0;
+	console.log(x);
+	fn();
+}
+nazad.onclick=function(){
+	x=x+100
+	if(x>0)x=-200;
+	console.log(x);
+	fn()
+}
+fn=function(){
+	poloska.style.left=x+"%";
+}
